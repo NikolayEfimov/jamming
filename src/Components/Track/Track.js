5a4baae1e7cb3eac5f271711ac3038a01ed4c77e
NikolayEfimov <nikolayefimov92@gmail.com>
2018-05-14 23:50:58 +0300

    add renderAction method

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -9,7 +9,7 @@ class Track extends Component {
           <h3><!-- track name will go here --></h3>
           <p><!-- track artist will go here--> | <!-- track album will go here --></p>
         </div>
-      <a className="Track-action"><!-- + or - will go here --></a>
+      <a className="Track-action">{this.renderAction()}</a>
       </div>
     );
   }
